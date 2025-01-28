7c9045fd470a8b8259e4407a754cb8ca4ce2d355
Adarsh <2020cs_adarshkpoojary_a@nie.ac.in>
2025-01-28 23:11:08 +0530

    Fixed gap in cards in Two Service pages

--- src/pages/EnggService.jsx
+++ src/pages/EnggService.jsx
@@ -24,13 +24,13 @@ const EnggService = () => {
       <div className='flex flex-wrap mt-10 lg:mt-20 '> 
          {enggLink.map((feature,index)=>(
             <div key={index} className='w-full sm:1/2 lg:w-1/3'>
-              <div className='flex'>
+              <div className='flex mb-10 '>
                 <div className='flex mx-6 h-10 w-10 bg-neutral-900 text-orange-700 justify-center items-center rounded-full'>
                     {feature.icon}
                 </div>
                 <div >
-                  <h5 className='mt-1 mb-6 text-xl'>{feature.text}</h5>
-                  <p className='text-md p-2 text-neutral-500'>{feature.description}</p>
+                  <h5 className='mt-1 mb-1 text-xl lg:mb-6'>{feature.text}</h5>
+                  <p className='text-md p-2 text-neutral-500 sm:mb-10'>{feature.description}</p>
                 </div>
               </div>
             </div>
--- src/pages/TestService.jsx
+++ src/pages/TestService.jsx
@@ -16,11 +16,11 @@ const TestService = () => {
     <SubNavbar />
     <div className="max-w-7xl max-h-5xl mx-auto pt-20 px-6">
          <div className='relative mt-5 min-h-[500px]'>
-      <div className="text-center">
+      <div className="text-center sm:mb-10">
         <span className='bg-neutral-900 text-orange-500 rounded-md h-6 text-lg font-medium px-2 py-1 uppercase'>
         Discover Our Services
         </span>
-        <h2 className='text-3xl sm:text-5xl lg:text-6xl mt-10 lg:mt-20 tracking-wide '>
+        <h2 className='text-3xl sm:text-5xl lg:text-6xl mt-10 lg:mt-20 tracking-wide'>
           Easily <span className='bg-gradient-to-r from-orange-400 to-orange-700 text-transparent bg-clip-text'>test</span> your code
         </h2>
       </div>
@@ -32,7 +32,7 @@ const TestService = () => {
                     {feature.icon}
                 </div>
                 <div >
-                  <h5 className='mt-1 mb-6 text-xl'>{feature.text}</h5>
+                  <h5 className='mt-1 mb-1 text-xl lg:mb-1 '>{feature.text}</h5>
                   <p className='text-md p-2 mb-20 text-neutral-500'>{feature.description}</p>
                 </div>
               </div>
