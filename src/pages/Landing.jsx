9669b251888cf51cfd4b559d48715c1aa5083d16
Adarsh <2020cs_adarshkpoojary_a@nie.ac.in>
2025-04-24 00:16:20 +0530

    view count fix 2

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -45,7 +45,7 @@ const Landing = () => {
 
   //view count in vercel
   useEffect(() => {
-    fetch('https://api.countapi.xyz/hit/testify2.vercel.app/')
+    fetch('https://api.countapi.xyz/hit/testify2-vercel/app')
       .then(res => res.json())
       .then(data => {
         console.log("Page views:", data.value);
